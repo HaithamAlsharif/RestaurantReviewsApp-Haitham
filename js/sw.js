d466d9e3491bebccabf0b389b9d69110131d5cca
Haitham Alsharif <mrhaithamalsharif@gmail.com>
2019-05-24 19:52:36 +0300

    media responisveness fix

--- js/sw.js
+++ js/sw.js
@@ -1,6 +1,8 @@
+let cacheName = 'myCache';
+
 self.addEventListener('install', function (event) {
     event.waitUntil(
-        caches.open('myCache').then(function (cache) {
+        caches.open(cacheName).then(function (cache) {
             return cache.addAll(
                 [
                     '/css/styles.css',
@@ -21,3 +23,20 @@ self.addEventListener('fetch', function (event) {
         })
     );
 });
+
+self.addEventListener('activate', function(event) {
+
+    var cacheWhitelist = ['myCashe'];
+  
+    event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.map(function(cacheName) {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
